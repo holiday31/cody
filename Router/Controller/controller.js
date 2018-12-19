d576f2c838b74998401068b099615a1567ac827b
HyeonJinPark <hjhch323@naver.com>
2018-12-19 20:24:44 +0900

    image 뜨도록 해봄!

--- Router/Controller/controller.js
+++ Router/Controller/controller.js
@@ -211,7 +211,7 @@ exports.postPOST = function(req, res)
     var _topy = req.body.topy;
     var _bottomx = req.body.bottomx;
     var _bottomy = req.body.bottomy;
-    var _imgurl='uploads/'+req.file.filename;
+    var _imgurl=req.file.filename;
     connection.query('insert into post values(null,?,?,?,?,?,?,?,?,?,?,0,0,0,0,0,0);'
         ,[_title,_content,_date,_userId,_typeId,_top,_bottom,_topurl,_bottomurl,_imgurl]//,_topx,_topy,_bottomx,_bottomy,like,dislike
         ,function(err){
